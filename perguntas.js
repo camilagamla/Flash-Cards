fa211597d32a7d9d178b3b96807281ae96a77bbe
Camila gamla <145797738+camilagamla@users.noreply.github.com>
2024-11-05 08:49:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'programação',
+    'Qual é o animal mais rápido do mundo?',
+    'O guepardo'
+)
+
+criaCartao(
+    'programação',
+    ' Qual é o planeta mais próximo do Sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'programação',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'programação',
+    'Qual é o maior oceano do mundo?',
+    ' Oceano Pacífico'
+)
+
+criaCartao(
+    'programação',
+    ' Qual é o elemento químico mais leve?',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'programação',
+    'Em que ano foi inventado o telefone?',
+    '1876'
+)
